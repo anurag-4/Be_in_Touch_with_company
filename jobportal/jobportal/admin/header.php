9f461b5e4cd394031194bb5ce12f55e42238df4b
Anurag Sharma <67834568+anurag-4@users.noreply.github.com>
2022-03-04 20:38:52 +0530

    final commit
    final commit

--- jobportal/jobportal/admin/header.php
+++
@@ -1,8 +0,0 @@
-<div style="width:1048px; height:120px; background-color:#EFEFEF;border:solid 0px #FF0000;">
-
-
-<div style=" width:1048px; height:80px; border:#339933 0px solid; font-size:36px" align="center">
-
-<!--logo-->PHPGURUKUL Job Portal Admin</div>
-
-</div>
